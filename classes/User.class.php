88366c2430e5edb22970781c0d7ab615badc7cc9
Arno De Decker <arnodedecker@telenet.be>
2016-03-22 14:56:03 +0100

    Added register function in user class

--- classes/User.class.php
+++ classes/User.class.php
@@ -2,10 +2,71 @@
 
 	class User
 	{
-		private $m_sFullname;
+		private $m_sFull_name;
 		private $m_sEmail;
-		private $m_sUsername;
 		private $m_sPassword;
+        private $m_sConfirm_password;
 	}
+
+    public function __set( $p_sProperty, $p_vValue )
+    {
+        switch($p_sProperty)
+        {
+            case 'FullName':
+                $this->m_sFull_name = $p_vValue;
+            break;
+            case 'Email':
+                $this->m_sEmail = $p_vValue;
+            break;
+            case 'Password':
+                $this->m_sPassword = $p_vValue;
+            break;
+            case 'ConfirmPassword':
+                $this->m_sConfirm_password = $p_vValue;
+            break;
+        } 
+    }
+
+    public function __get($p_sProperty)
+    {
+        switch($p_sProperty)
+        {
+            case 'FullName':
+                return($this->m_sFull_name);
+            break;
+            case 'Email':
+                return($this->m_sEmail);
+            break;
+            case 'Password':
+                return($this->m_sPassword);
+            break;
+            case 'ConfirmPassword':
+                return($this->m_sConfirm_password);
+            break;
+        }
+    } 
+
+    function Register()
+    {
+        $options = $options = [
+            'cost' => 12,    
+        ];
+
+        // connectie
+        $connection = new mysqli("localhost", "root", "root", "IMDstagram");
+        if($connection -> connect_errno)
+        {
+            die("No database connection");
+        }
+
+        // query
+        $query = "INSERT INTO users(full_name, email, password, confirm_password) VALUES ('$full_name', '$email', '$password', '$confirm_password');";
+
+        if($connection -> query($query))
+        {
+            $success = "Welcome to IMDstagram!";
+        }
+        
+    }
 
 ?>
--- views/index.php
+++ views/index.php
@@ -10,9 +10,9 @@
 		<div class="login">
 			
 			<form action="" method="post">
-				<label for="email">E-mail</label>
+				<label for="login_email">E-mail</label>
 				<input type="text" name="login_email" class="textfield" placeholder="E-mail">
-				<label for="password">Password</label>
+				<label for="login_password">Password</label>
 				<input type="password" name="login_password" class="textfield" placeholder="Password">
 				<input type="submit" class="button" value="Log in">
 			</form>
