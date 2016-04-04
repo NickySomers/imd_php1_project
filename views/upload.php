45a2c397f8a80423ae3ac0a94657aae3a877b329
NickySomers <nicky@artcoresociety.com>
2016-04-04 17:59:50 +0200

    Updated the upload page style

--- views/upload.php
+++ views/upload.php
@@ -13,23 +13,13 @@
         <meta charset="utf-8">
         <link rel="stylesheet" href="../css/style.css">
         <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-        <script src="../bower_components/adaptive.background/src/jquery.adaptive-backgrounds.js"></script>
-        <script>
-            $(document).ready(function () {
-                $.adaptiveBackground.run({
-                    parent: '.image-container'
-                })
-            });
-        </script>
     </head>
 
     <body>
         <header>
             <div class="logo"></div>
         </header>
-        <div class="wrapper">
+        <div>
 
             <?php 
                 $photo = new Photo();
@@ -48,7 +38,7 @@
 
 
                 <div class="container-fluid">
-                    <div class="col-md-8 image-container">
+                    <div class="image-container">
 
                         <?php if(empty($image)): ?>
 
@@ -61,46 +51,54 @@
                             <?php else: ?>
 
                                 <img class="image" src="<?php echo $image ?>">
-                                <div class="image-background" style="background-image: url(<?php echo $image; ?>)"></div>
+                                <div class="add-image-info">
+                                    <div class="content">
+                                        <form action="" method="post" id="publish-form">
+                                            <input type="text" name="image" hidden value="<?php echo $image; ?>">
+                                            <label for="description">Description</label>
+                                            <textarea name="description" class="image-description-field"></textarea>
+
+                                        </form>
+                                        <div class="button-group">
+
+                                            <button type="button" class="custom-button gray">Another photo</button>
+                                            <button type="button" id="publish" class="custom-button">Publish</button>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="image-background" style="background-image: url(<?php echo $image; ?>)">
+
+
+
+
+
+
+
+                                </div>
+
+
 
                                 <?php endif; ?>
 
                     </div>
 
-                    <div class="col-md-4 add-image-info">
-                        <?php if(!empty($image)): ?>
-
-                            <form action="" method="post" id="publish-form">
-                                <input type="text" name="image" hidden value="<?php echo $image; ?>">
-                                <label for="description">Description:</label>
-                                <textarea name="description" class="image-description-field"></textarea>
-
-                            </form>
-                            <div class="button-group">
-
-                                <button type="button" class="custom-button">Select another photo</button>
-                                <button type="button" id="publish" class="custom-button">Publish</button>
-
-                            </div>
-
-                            <?php endif; ?>
-                    </div>
 
                 </div>
 
         </div>
 
-
-
-
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
         <script>
-            
-            $("#publish").click(function(){
-            
+            $("#publish").click(function () {
+
                 $("#publish-form").submit();
-                
+
             })
-            
+
             $('#form-upload').submit(function () {
                 var queryString = new FormData($('form')[0]);
                 $.ajax({
