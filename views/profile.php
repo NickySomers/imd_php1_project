8a2de5bdde2f2fed4833d08205fcb2d99c2888cf
NickySomers <somers.nicky@gmail.com>
2016-04-07 18:50:15 +0200

    Created an html structure for the profile page

---
+++ views/profile.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>IMDstagram</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <div class="logo"></div>
+    </header>
+
+    <div class="profile-header">
+        <div class="wrapper">
+
+            <div class="profile-picture"></div>
+            <div>
+                <h1>Nicky Somers</h1>
+                <h2>@SomersNicky</h2>
+            </div>
+        </div>
+
+        <div class="overlay"></div>
+    </div>
+    <div class="profile-grid container-fluid">
+        <div class="wrapper">
+            <div class="col-md-4 profile-grid-image-container"><div class="profile-grid-image">Test</div></div>
+            <div class="col-md-4 profile-grid-image-container"><div class="profile-grid-image">Test</div></div>
+            <div class="col-md-4 profile-grid-image-container"><div class="profile-grid-image">Test</div></div>
+            <div class="col-md-4 profile-grid-image-container"><div class="profile-grid-image">Test</div></div>
+            <div class="col-md-4 profile-grid-image-container"><div class="profile-grid-image">Test</div></div>
+            <div class="col-md-4 profile-grid-image-container"><div class="profile-grid-image">Test</div></div>
+            <div class="col-md-4 profile-grid-image-container"><div class="profile-grid-image">Test</div></div>
+
+        </div>
+
+    </div>
+</body>
+
+</html>
