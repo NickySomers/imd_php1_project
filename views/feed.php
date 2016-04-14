302220b2022304fe4042a01c694741ed19d09d49
Arno De Decker <arnodedecker@telenet.be>
2016-04-14 13:58:49 +0200

    Started feed.php page

---
+++ views/feed.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>IMDstagram</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+
+        <body>
+            <header>
+                <div class="logo"></div>
+            </header>
+
+            <div class="wrap-photo">
+                <div class="header-photo">
+                    
+                </div>
+            </div>
+            
+        </body>
+
+    </html>
