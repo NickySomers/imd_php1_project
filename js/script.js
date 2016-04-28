683f94b730e5a052d6b7ebcabba04de387dbb7ae
nickysomers <somers.nicky@gmail.com>
2016-04-28 07:08:34 +0200

    Fixed the Ajax lazy loading bug

--- js/script.js
+++ js/script.js
@@ -1,24 +1,1 @@
-$(document).ready(function(){
-
-    /* FEED PAGE LIKE BUTTON */
-    $(".liken").click(function(){
-        $(this).css("background-image", "url('../images/heart-fill.svg')");
-    });
-    
-    /* LAZY LOADING FEED PAGE */
-    $(document).on('click','.show_more',function(){
-        var ID = $(this).attr('id');
-        $('.show_more').hide();
-        $('.loding').show();
-        $.ajax({
-            type:'POST',
-            url:'feed.php',
-            data:'id='+ID,
-            success:function(html){
-                $('#show_more_main'+ID).remove();
-                $('.wrap-photo').append(html);
-            }
-        }); 
-    });
-});
-
+$(document).ready(function(){    /* FEED PAGE LIKE BUTTON */    $(".liken").click(function(){        $(this).css("background-image", "url('../images/heart-fill.svg')");    });        /* LAZY LOADING FEED PAGE */    $(window).scroll(function() {   if($(window).scrollTop() + $(window).height() == $(document).height()) {        setTimeout(function(){        var data = {            index: $(".show_more").attr('data-index')        }        $.post("loadFeed.php", data, function(response) {                        $('.show_more_main').remove();            var data = $.parseJSON(response);            for(var i = 1; i < data.length; i++){                var header_content = '<div class="profile-pic"></div><div class="profile-name">'+"TEST"+'</div><div class="minutes-posted">'+"DATE"+'</div>';                var header = '<div class="header-photo">'+header_content+'</div>';                                var image = '<img src="'+data[i][0]+'" alt="Photo" width="100%" height="auto">';                var footer_content = '<div class="likes">test</div><div class="wrap-description"><div class="description-username">USERNAME</div><div class="description-text">"'+data[i][1]+'"</div></div><div class="line"></div>';                var footer = '<div class="footer-photo">'+footer_content+'</div>';                var post = '<div class="wrap-photo">' + header + image + footer + '</div>';                              $('.container').append(post);            }            // if($(".show_more").attr('data-index') == null){            //     alert("Test");            // }else{                var showmore = '<div class="show_more_main" id="show_more_main"><span data-index="'+data[0]+'" class="show_more" title="Load more posts">Show more</span><span class="loding" style="display: none;"><span class="loding_txt">Loading....</span></span></div>';                $('.container').append(showmore);            // }                    });}, 1000);   }});});
