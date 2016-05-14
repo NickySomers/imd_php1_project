8d81fe742c84fa7a7f7cd9a706ba2027b7e3a086
Arno De Decker <arnodedecker@telenet.be>
2016-05-14 12:37:58 +0200

    Filters fully working

--- views/feed.php
+++ views/feed.php
@@ -10,73 +10,83 @@
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $userN = $_SESSION['user'];
-    $posts = $conn->query("SELECT p . *, u . *, p.description pdescription, p.id pid FROM users_followers uf, posts p, users u WHERE uf.followUserId = '$userN' AND uf.userId = p.userId AND uf.userId = u.id ORDER BY p.id DESC LIMIT 2");
+    $posts = $conn->query("SELECT p . *, u . *, p.description pdescription, p.id pid, p.filter pfilt FROM users_followers uf, posts p, users u WHERE uf.followUserId = '$userN' AND uf.userId = p.userId AND uf.userId = u.id ORDER BY p.id DESC LIMIT 2");
             
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <title>IMDstagram</title>
-        <link rel="stylesheet" href="../css/style.css">
-        <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    </head>
-
-        <body>
+<head>
+    <meta charset="UTF-8">
+    <title>IMDstagram</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/cssgram.min.css">
+</head>
+<body>
            
-            <header>
-                <div class="logo"></div>
-            </header>
+    <?php include_once("header.php"); ?>
             
-            <div class="container">
+    <div class="container">
 
-                    <?php
-                    //number of rows
-                    $rowCount = $posts->rowCount();
+        <?php
+
+        //number of rows
+        $rowCount = $posts->rowCount();
+
+        while($row = $posts->fetch(PDO::FETCH_ASSOC))
+        {
+            $id = $row['pid'];
+
+        ?>
+            <div class="wrap-photo" data-index="<?php echo $row['pid']; ?>">
+                <div class="header-photo">
+                    <div class="profile-pic"></div>
+                    <div class="profile-name"><?php echo $row['username']; ?></div>
+                    <div class="minutes-posted"><?php echo $row['date']; ?></div>
+                </div>
+                
+                <?php if(!empty($row['pfilt'])): ?>
                     
+                    <figure class="<?php echo $row['pfilt'] ?>">
+                        <img src="<?php echo $row['picturePath']; ?>" alt="Photo" width="100%" height="auto">
+                    </figure>
+                    
+                <?php else: ?>
                    
-                        while($row = $posts->fetch(PDO::FETCH_ASSOC))
-                        { 
-
-                            $id = $row['pid'];
-
-                    ?>
-                        <div class="wrap-photo" data-index="<?php echo $row['pid']; ?>">
-                            <div class="header-photo">
-                                <div class="profile-pic"></div>
-                                <div class="profile-name"><?php echo $row['username']; ?></div>
-                                <div class="minutes-posted"><?php echo $row['date']; ?></div>
-                            </div>
-                            <img src="<?php echo $row['picturePath']; ?>" alt="Photo" width="100%" height="auto">
-                            <div class="footer-photo">
-                                <div class="likes">test</div>
-                                <div class="wrap-description">
-                                    <div class="description-username"><?php echo $row['username']; ?></div>
-                                    <div class="description-text"><?php echo $row['pdescription']; ?></div>
-                                </div>
-                                <div class="line"></div>
-                                <div class="wrap-liken">
-                                    <div class="liken"></div>
-                                    <input type="text" name="comment" class="comment" placeholder="Add a comment...">
-                                    <div class="dots"></div>
-                                </div>
-                            </div>
-                        </div>
-
-                    <?php } ?>
-
-                    <div class="show_more_main" id="show_more_main">
-                        <span data-index="<?php echo $id; ?>" class="show_more" title="Load more posts">Show more</span>
-                        <span class="loding" style="display: none;"><span class="loding_txt">Loading....</span></span>
+                        <img src="<?php echo $row['picturePath']; ?>" alt="Photo" width="100%" height="auto">
+                
+                <?php endif; ?>
+                
+                <div class="footer-photo">
+                    <div class="likes">test</div>
+                    <div class="wrap-description">
+                        <div class="description-username"><?php echo $row['username']; ?></div>
+                        <div class="description-text"><?php echo $row['pdescription']; ?></div>
                     </div>
-
-      
+                    <div class="line"></div>
+                    <div class="wrap-liken">
+                        <div class="liken"></div>
+                        <input type="text" name="comment" class="comment" placeholder="Add a comment...">
+                        <div class="dots"></div>
+                    </div>
+                </div>
             </div>
-            
-            <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
-            <script src="../js/script.js"></script>
-            
-        </body>
 
+        <?php 
+
+        } 
+
+        ?>
+
+        <div class="show_more_main" id="show_more_main">
+            <span data-index="<?php echo $id; ?>" class="show_more" title="Load more posts">Show more</span>
+            <span class="loding" style="display: none;"><span class="loding_txt">Loading...</span></span>
+        </div>
+      
+    </div> <!-- END container -->
+      
+    <!-- JAVASCRIPT AND JQUERY -->      
+    <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
+    <script src="../js/script.js"></script>
+            
+</body>
 </html>
--- views/profile.php
+++ views/profile.php
@@ -1,33 +1,31 @@
 <?php 
 
-    if(empty($_GET['id'])){
-        
+    if(empty($_GET['id']))
+    {
         header("Location: index.php");
-        
     }
-session_start();
+    
+    session_start();
 
-include_once("../classes/User.class.php");
+    include_once("../classes/User.class.php");
 
-$conn = new PDO('mysql:host=localhost;dbname=imdstagram', "root", "root");
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
+    $conn = new PDO('mysql:host=localhost;dbname=imdstagram', "root", "root");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <title>IMDstagram</title>
-        <link rel="stylesheet" href="../css/style.css">
-        <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    </head>
-    <?php
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>IMDstagram</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/cssgram.min.css">
+</head>
+<?php
   
     //CHANGE TO FUNCTION IN USER CLASS
-    try {
-
+    try 
+    {
         $conn = new PDO('mysql:host=localhost;dbname=imdstagram', "root", "root");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $data = $conn->query("SELECT * FROM posts WHERE userId = ".$_GET['id']." ORDER BY date DESC"); 
@@ -35,124 +33,116 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $followingdata = $conn->query("SELECT * FROM users_followers WHERE userId = ".$_GET['id']); 
         $followersdata = $conn->query("SELECT * FROM users_followers WHERE followUserId = ".$_GET['id']); 
         
-        foreach ($userdata as $row) {
-
+        foreach ($userdata as $row) 
+        {
             $name = $row['firstname'] . " " . $row['lastname'];
             $username = $row['username'];
-            $profile = $row['profilePicture'];
-            
+            $profile = $row['profilePicture'];   
         }
+        
         $count_photos = $data->rowCount();
         $count_followers = $followersdata->rowCount();
         $count_following = $followingdata->rowCount();
 
-    } catch(PDOException $e) {
+    } 
+    catch(PDOException $e) 
+    {
         echo 'ERROR: ' . $e->getMessage();
     }
     
 ?>
+<body>
+    
+    <?php include_once("header.php"); ?>
 
-        <body>
-        <?php include_once("header.php"); ?>
-            <header>
-                <div class="logo"></div>
-            </header>
-
-            <div class="profile-header">
-                <div class="wrapper">
-
-
-                    <div class="profile-picture" style="background-image: url(<?php echo $profile; ?>)"></div>
-
-                    <div>
-                        <h1><?php echo $name; ?></h1>
-                        <h2>@<?php echo $username; ?></h2>
-                    </div>
-
-
-                </div>
-
-                <div class="profile-information">
-                    <div class="profile-information-item">
-                        <span>Photos</span>
-                        <span class="amount"><?php echo $count_photos; ?></span>
-                    </div>
-                    <div class="profile-information-item">
-                        <span>Followers</span>
-                        <span class="amount"><?php echo $count_followers; ?></span>
-                    </div>
-                    <div class="profile-information-item">
-                        <span>Following</span>
-                        <span class="amount"><?php echo $count_following; ?></span>
-                    </div>
-                    <div class="profileFollow">
-                        <?php
-
-                        $followed = $conn->query("SELECT COUNT(*) As test FROM users_followers WHERE userId = 21 AND followUserId = 2 ");
-                        $result= $followed->fetch(PDO::FETCH_ASSOC);
-
-                        if($result['test'] == 0){
-                            echo'<input id="follow" name="follow" type="submit" value="follow" >';
-                            echo '<input id="unfollow" name="following" type="submit" value="following" style="display:none;">';
-                        }
-                        elseif($result['test'] == 1){
-                            echo '<input id="unfollow" name="following" type="submit" value="following">';
-                            echo'<input id="follow" name="follow" type="submit" value="follow" style="display: none">';
-                        }
-
-                        ?>
-
-                    </div>
-                </div>
-
-
-
-                <div class="overlay"></div>
-                </div>
-            <div class="profile-grid container-fluid">
-                <div class="wrapper">
-
-                    <?php
-
-                    foreach ($data as $row) {
-                        
-                        echo '<div class="col-md-4 profile-grid-image-container"><div class="profile-grid-image" style="background-image: url('.$row['picturePath'].')">Test</div></div>';
-                    }
-                
-                ?>
-
-                </div>
-
+    <div class="profile-header">
+        
+        <div class="wrapper">
+            <div class="profile-picture" style="background-image: url(<?php echo $profile; ?>)"></div>
+            <div>
+                <h1><?php echo $name; ?></h1>
+                <h2>@<?php echo $username; ?></h2>
             </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script>
-            $("#follow").click(function () {
-                $.ajax({
-                    url: "../ajax/followProfile.php",
-                    method:"POST",
-                    data:{ userId: <?php echo $_GET['id'] ?> }
-                }).done(function() {
+        </div>
 
-                    $("#follow").hide();
-                    $("#unfollow").show();
-                });
+        <div class="profile-information">
+            <div class="profile-information-item">
+                <span>Photos</span>
+                <span class="amount"><?php echo $count_photos; ?></span>
+            </div>
+            <div class="profile-information-item">
+                <span>Followers</span>
+                <span class="amount"><?php echo $count_followers; ?></span>
+            </div>
+            <div class="profile-information-item">
+                <span>Following</span>
+                <span class="amount"><?php echo $count_following; ?></span>
+            </div>
+            <div class="profileFollow">
+                <?php
 
-            });$("#unfollow").click(function () {
-                $.ajax({
-                    url: "../ajax/unfollowProfile.php",
-                    method:"POST",
-                    data:{ userId: <?php echo $_GET['id'] ?> }
-                }).done(function() {
-                    //alert("het werkt");
-                    $("#unfollow").hide();
-                    $("#follow").show();
-                });
+                $followed = $conn->query("SELECT COUNT(*) As test FROM users_followers WHERE userId = 21 AND followUserId = 2 ");
+                $result= $followed->fetch(PDO::FETCH_ASSOC);
 
+                if($result['test'] == 0)
+                {
+                    echo'<input id="follow" name="follow" type="submit" value="follow" >';
+                    echo '<input id="unfollow" name="following" type="submit" value="following" style="display:none;">';
+                }
+                elseif($result['test'] == 1)
+                {
+                    echo '<input id="unfollow" name="following" type="submit" value="following">';
+                    echo'<input id="follow" name="follow" type="submit" value="follow" style="display: none">';
+                }
+
+                ?>
+            </div>
+        </div>
+        
+        <div class="overlay"></div>
+        
+    </div> <!-- END profile-header -->
+    
+    <div class="profile-grid container-fluid">
+        
+        <div class="wrapper">
+            <?php
+
+            foreach ($data as $row) 
+            {
+                echo '<div class="col-md-4 profile-grid-image-container"><div class="profile-grid-image '.$row['filter'].'" style="background-image: url('.$row['picturePath'].')"></div></div>';
+            }
+
+            ?>
+
+        </div>
+
+    </div><!-- END profile-grid container-fluid -->
+    
+    <!-- JAVASCRIPT AND JQUERY -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>
+        $("#follow").click(function () {
+            $.ajax({
+                url: "../ajax/followProfile.php",
+                method:"POST",
+                data:{ userId: <?php echo $_GET['id'] ?> }
+            }).done(function() {
+                $("#follow").hide();
+                $("#unfollow").show();
             });
+        });$("#unfollow").click(function () {
+            $.ajax({
+                url: "../ajax/unfollowProfile.php",
+                method:"POST",
+                data:{ userId: <?php echo $_GET['id'] ?> }
+            }).done(function() {
+                //alert("het werkt");
+                $("#unfollow").hide();
+                $("#follow").show();
+            });
+        });
+    </script>
 
-
-        </script>
-
-        </body>
-
-    </html>
+</body>
+</html>
